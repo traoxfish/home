e405c59ff1b5242822cab4be043ab303ddc62b36
TraoX1 <mhettwer37@gmail.com>
2022-04-29 21:20:15 -0700

    asd
    asd

--- fish/fish.js
+++ fish/fish.js
@@ -4,6 +4,7 @@ function test() {
     fetch('https://traoxfish.us-3.evennode.com', {
     method: 'POST',
     headers: {
+        'Orgin': 'http://localhost:5000',
         'Content-Type': 'application/json',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin'
