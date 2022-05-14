9c8d4b215892cbcf2b47daf2d5b49fe716ac47f7
TraoX1 <mhettwer37@gmail.com>
2022-05-13 17:44:30 -0700

    asdasd
    asdasd

--- browser.js
+++ browser.js
@@ -1,17 +1,36 @@
 function search() {
     if(event.keyCode == 13) {
-        var location = document.getElementById("url").value;
         
-        fetch(location, {
+        var location = document.getElementById("url").value;
+
+        fetch('https://traoxfish.us-3.evennode.com/leaderboards', {
             method: 'GET',
             credentials: "same-origin",
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'No-Store'
+            },
         }).then(response => {
-    
+            return response.json();
+        }).then(json => {
+
+        });
+
+        fetch('http://traoxfish.us-3.evennode.com/browse', {
+            method: 'POST',
+            credentials: "same-origin",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                url: location
+            })
+        }).then(response => {
+
             return response.text();
         }).then(text => {
             console.log(text)
         });
-
 
     }
 }
