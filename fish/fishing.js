99b49f13931701b855ad0395d6d68e0f058fd831
TraoX <64757239+TraoX1@users.noreply.github.com>
2024-01-10 10:54:02 -0800

    Update fishing.js

--- fish/fishing.js
+++ fish/fishing.js
@@ -314,7 +314,8 @@ function getMessages(first) {
                     messageElement.style.fontSize = "calc(0.66vw + 6px)"
                     messageElement.textContent = chat[message]
                     var username = chat[message].split("M", 2)[1].split(":", 2)[0].replaceAll(" ", "")
-                    messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\">" + username + "</p>")
+                    messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p id=\"message" + message + "\" onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\"></p>")
+                    document.getElementById("message" + message).innerText = username
                     messageElement.innerHTML = messageElement.innerHTML.replace("@" + getCookie("username"), "<p style=\"color: #ea7b7b; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\" >" + "@" + getCookie("username") + "</p>")
                     document.getElementById("chat").appendChild(messageElement)
                 }
@@ -324,7 +325,8 @@ function getMessages(first) {
                     if (messageElement.textContent != chat[message]) {
                         messageElement.textContent = chat[message]
                         var username = chat[message].split("M", 2)[1].split(":", 2)[0].replaceAll(" ", "")
-                        messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\">" + username + "</p>")
+                        messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p id=\"message" + message + "\" onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\"></p>")
+                        document.getElementById("message" + message).innerText = username
                         messageElement.innerHTML = messageElement.innerHTML.replace("@" + getCookie("username"), "<p style=\"color: #ea7b7b; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\" >" + "@" + getCookie("username") + "</p>")
                     }
                 }
