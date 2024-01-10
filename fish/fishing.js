22036553795d7cfed5a6e55f5930fcb6db1b5996
TraoX <64757239+TraoX1@users.noreply.github.com>
2024-01-10 09:54:54 -0800

    Update fishing.js

--- fish/fishing.js
+++ fish/fishing.js
@@ -315,6 +315,7 @@ function getMessages(first) {
                     messageElement.textContent = chat[message]
                     var username = chat[message].split("M", 2)[1].split(":", 2)[0].replaceAll(" ", "")
                     messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\">" + username + "</p>")
+                    messageElement.innerHTML = messageElement.innerHTML.replace("@" + username, "<p style=\"color: #ea7b7b; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\" >" + "@" + username + "</p>")
                     document.getElementById("chat").appendChild(messageElement)
                 }
             } else {
@@ -323,7 +324,7 @@ function getMessages(first) {
                     if (messageElement.textContent != chat[message]) {
                         messageElement.textContent = chat[message]
                         var username = chat[message].split("M", 2)[1].split(":", 2)[0].replaceAll(" ", "")
-                    messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\">" + username + "</p>")
+                        messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\">" + username + "</p>")
                     }
                 }
             }
