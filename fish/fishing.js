211b20cc5745bab73d73dd5e7176476ac3eac638
TraoX <64757239+TraoX1@users.noreply.github.com>
2024-01-10 11:01:04 -0800

    Update fishing.js

--- fish/fishing.js
+++ fish/fishing.js
@@ -315,9 +315,9 @@ function getMessages(first) {
                     messageElement.textContent = chat[message]
                     var username = chat[message].split("M", 2)[1].split(":", 2)[0].replaceAll(" ", "")
                     messageElement.innerHTML = messageElement.innerHTML.replace(username, "<p id=\"message" + message + "\" onclick=\"viewProfile(\'" + username + "\')\" style=\"cursor: pointer; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\"></p>")
-                    document.getElementById("message" + message).innerText = username
                     messageElement.innerHTML = messageElement.innerHTML.replace("@" + getCookie("username"), "<p style=\"color: #ea7b7b; display: inline-block; margin-bottom: 0px; margin-top: 0px; max-width: 99%; font-size: calc(6px + 0.66vw);\" >" + "@" + getCookie("username") + "</p>")
                     document.getElementById("chat").appendChild(messageElement)
+                    document.getElementById("message" + message).innerText = username
                 }
             } else {
                 for (var message in chat.reverse()) {
